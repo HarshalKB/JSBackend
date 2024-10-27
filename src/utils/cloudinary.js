f28a4a6b3fee528b9b851d0096915c7d577296aa
Harshal Bombarde <harshalbombarde@gmail.com>
2024-10-27 17:00:13 +0530

    added login, logout and auth

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,8 +1,6 @@
 import {v2 as cloudinary} from "cloudinary";
 import fs from "fs";
 
-import { v2 as cloudinary } from 'cloudinary';
-
 cloudinary.config({ 
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
     api_key: process.env.CLOUDINARY_API_KEY, 
@@ -17,6 +15,7 @@ const uploadOnCloudinary = async (localFilePath) => {
             resource_type: "auto"
         })
         console.log("file uploaded on cloudinary", response.url);
+        fs.unlinkSync(localFilePath);
         return response
     } catch (error) {
         fs.unlinkSync(localFilePath) // remove the locally saved temp file
